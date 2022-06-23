957cb1af7c8bf440799f1b60069077574850f432
Datsuk Maxim <datsuk.job@gmail.com>
2022-06-24 02:04:47 +0700

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Maxim Datsuk on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
